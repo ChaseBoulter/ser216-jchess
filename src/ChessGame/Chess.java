a6028008c564faf73d67042e1b741c5cfec86870
jgoralcz <jgoralcz@asu.edu>
2018-04-10 13:44:03 -0700

    renamed ChessGame package to chessgame.

--- src/ChessGame/Chess.java
+++
@@ -1,9 +0,0 @@
-package chessgame;
-
-import mainframe.chessframe.MainFrame;
-
-public class Chess {
-    public static void main(String[] args) {
-        MainFrame frame = new MainFrame();
-    }
-}
